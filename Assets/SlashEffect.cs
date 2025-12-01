b831c9e46a201b52079ed860583ec2665acc7fba
Jachowaty <Jachowaty@gmail.com>
2025-12-01 20:49:09 +0100

    Attack

--- Assets/SlashEffect.cs
+++ Assets/SlashEffect.cs
@@ -1,3 +1,4 @@
+// SlashEffect.cs
 using UnityEngine;
 
 public class SlashEffect : MonoBehaviour
@@ -5,18 +6,24 @@ public class SlashEffect : MonoBehaviour
     public float growSpeed = 15f;
     public float fadeSpeed = 8f;
     
+    // NOWA ZMIENNA: Prędkość przesuwania się ataku
+    public float moveSpeed = 5f; 
+
     private SpriteRenderer spriteRenderer;
     private Vector3 targetScale;
-    
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         targetScale = transform.localScale;
         transform.localScale = targetScale * 0.5f;
     }
-    
+
     void Update()
     {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        
         transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * growSpeed);
         
         Color color = spriteRenderer.color;
